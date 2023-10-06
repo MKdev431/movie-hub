9d96a92712b8e5341a2ac9e71b82ff331c4a54bb
Michal Kurek (Mike) <michal@kurek.pl>
2023-10-06 12:01:26 +0200

    basic project structure finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import GlobalStyles from "./components/styled/Global";
+import { ThemeProvider } from "styled-components";
+import Nav from "./components/Nav";
+import Pages from "./Pages";
+
+const theme = {
+  colors: {
+    body: "#c7c7c7",
+  },
+};
+
 function App() {
   return (
-    <div className="app">
-      <h1>it works</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <GlobalStyles />
+        <Nav />
+        <Pages />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Pages.jsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyAccount from "./pages/MyAccount";
+
+function Pages() {
+  return (
+    <>
+      <Home />
+      <Login />
+      <MyAccount />
+    </>
+  );
+}
+
+export default Pages;
